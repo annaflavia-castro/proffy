5be13dd4873a4cf08265dd329b90232fdd0b6ce4
Anna Flávia <anna.castro@truckpad.com.br>
2020-08-13 00:49:32 -0300

    Card list customization responsive

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -29,32 +28,11 @@ function TeacherList() {
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img src="https://avatars0.githubusercontent.com/u/50669648?s=460&u=aa382f2fbb227a2a68a2f3c157bf5d6e6d152ea4&v=4" alt="Anna Flávia Castro"/>
-            <div>
-              <strong>Anna Flávia Castro</strong>
-              <span>Matemática</span>
-            </div>
-          </header>
-
-          <p>
-            Entusiasta das melhores tecnologias de matemática avançada.
-            <br />
-            Apaixonada por resolver problemas matemáticos e por mudar a vida das pessoas através de soluções. Mais de 200.000 pessoas já se beneficiaram das minhas soluções.
-          </p>
-
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 50,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="WhatsApp"/>
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </div>
   );
